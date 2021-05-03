70cbf979eca680020ebd6a0a0fa713e42939577d
Melinda <melinda.sava.td@gmail.com>
2021-05-03 13:43:56 +0200

    fixed post with vowel count in review, graded by Kristian

--- app.js
+++ app.js
@@ -10,17 +10,27 @@ const _ = require('lodash');
 //stored one express instance in app
 const app = express();
 
+//importing body parser
+const bodyParser = require("body-parser");
+
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 //app listening on port 3000
 app.listen(3000);
 
 //setting hompage to return quotes object
+app.use(express.static('public'));
+/* //previous way to return quotes object
 app.get('/', (req, res) => {
 
     //setting status to file sent https://stackoverflow.com/questions/26066785/proper-way-to-set-response-status-and-json-content-in-a-rest-api-made-with-nodej
     res.status(200).sendFile('data.json', { root: __dirname + '/public' });
 })
+*/
 
-//api for counting the vowels within a string
+//api for counting the vowels within a string sent in the url
 app.get(`/api/count-vowels/:string`, (req, res) => {
 
     //storing sent string in text
@@ -36,6 +46,23 @@ app.get(`/api/count-vowels/:string`, (req, res) => {
     res.status(200).send({ text, vowels: getVowels(text) });
 })
 
+//api for counting for counting the vowels within a string sent in a post request body
+app.post(`/api/count-vowels`, (req, res) => {
+
+    //storing sent string in text
+    console.log(req.body.text);
+    const text = req.body.text;
+
+    //small regex that counts vowels: https://stackoverflow.com/questions/29450399/counting-number-of-vowels-in-a-string-with-javascript
+    function getVowels(str) {
+        var m = str.match(/[aeiou]/gi);
+        return m === null ? 0 : m.length;
+    }
+
+    //sending a json object containing the entered text and the number of vowels in it
+    res.send({ text, vowels: getVowels(text) });
+})
+
 //api for random number between 0 and 1023
 app.get('/api/random', (req, res) => {
 
@@ -43,7 +70,7 @@ app.get('/api/random', (req, res) => {
     const number = _.random(0, 1023);
 
     //sending a random number between 0 and 1023
-    res.status(200).send({ number });
+    res.send({ number, haveFun: 'yes', newThing: 'keep going' });
 })
 
 //api for custom random number between 0 and requested num https://www.robinwieruch.de/node-express-server-rest-api
